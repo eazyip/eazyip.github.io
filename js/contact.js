d737f2401be3f7c4a4f4492353472e3c26ac9fff
medilies <medilies@gmail.com>
2021-01-09 15:03:40 +0100

    diable contacting

--- js/contact.js
+++ js/contact.js
@@ -2,34 +2,35 @@ const contactForm = document.querySelector("#contact-form");
 const contactMsgStatus = document.querySelector("#js-contact-msg-status");
 const contactSubmitBtn = document.querySelector("#js-contact-submit-btn");
 
-contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (contactSubmitBtn.classList.contains("disabled-btn")) return;
+// Disable contact
+contactSubmitBtn.classList.add("disabled-btn");
+contactMsgStatus.innerHTML =
+    "<p class='alarming-text'>This option is unavailable! please try another medium to pass your massege (An email maybe)</p>";
 
-    contactSubmitBtn.classList.add("disabled-btn");
+// contactForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     if (contactSubmitBtn.classList.contains("disabled-btn")) return;
 
-    const formData = new FormData(contactForm);
+//     contactSubmitBtn.classList.add("disabled-btn");
 
-    // const name = formData.get("name");
-    // const email = formData.get("email");
-    // const msg = formData.get("msg");
+//     const formData = new FormData(contactForm);
 
-    fetch(`${window.location.origin}/apis/contact`, {
-        method: "post",
-        body: formData,
-    })
-        .then((res) => res.text())
-        .then((text) => {
-            if (text === "nice msg") {
-                contactMsgStatus.innerHTML =
-                    "<p class='success-text'>We recieved your message. Thank you :)</p>";
-                contactSubmitBtn.classList.remove("disabled-btn");
-            } else if (text !== "nice msg") {
-                throw "backend err";
-            }
-        })
-        .catch(() => {
-            contactMsgStatus.innerHTML =
-                "<p class='alarming-text'>A server error happened! please try another medium to pass your massege (An email maybe)</p>";
-        });
-});
+//     fetch(`${window.location.origin}/apis/contact`, {
+//         method: "post",
+//         body: formData,
+//     })
+//         .then((res) => res.text())
+//         .then((text) => {
+//             if (text === "nice msg") {
+//                 contactMsgStatus.innerHTML =
+//                     "<p class='success-text'>We recieved your message. Thank you :)</p>";
+//                 contactSubmitBtn.classList.remove("disabled-btn");
+//             } else if (text !== "nice msg") {
+//                 throw "backend err";
+//             }
+//         })
+//         .catch(() => {
+//             contactMsgStatus.innerHTML =
+//                 "<p class='alarming-text'>A server error happened! please try another medium to pass your massege (An email maybe)</p>";
+//         });
+// });
